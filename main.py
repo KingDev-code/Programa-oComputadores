a4918bd891feabaa82e861118ee7e00177e97819
KingDev-code <davidavirodrigues27@gmail.com>
2024-03-12 10:50:13 -0300

    Primeira aula

---
+++ main.py
@@ -0,0 +1,14 @@
+# Apresentação do programa
+print('Vamos fazer um programa que calcula a média do aluno')
+
+# Entrada de Dados
+n1 = float(input('Digite a primeira nota: '))
+n2 = float(input('Digite a segunda nota: '))
+n3 = float(input('Digite a terceira nota: '))
+n4 = float(input('Digite a quarta nota: '))
+
+# Calculo dos dados fornecidos
+Ma = (n1 + n2 + n3 + n4) / 4
+
+# Saida de dados
+print('A media do aluno é {}'.format(Ma))
--- py.py
+++
@@ -1,4 +0,0 @@
-n = int(input("Quanto é o seu salario? R$"))
-s = n * (10/100)
-v = n + s
-print('Seu salario com 10% de aumento é {}'.format(v))
